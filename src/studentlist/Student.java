e3110b34289d1f399d9388f81597a6d4ea594cb0
Sharif <97964772+kulmie2@users.noreply.github.com>
2022-01-25 09:30:36 -0500

    Update Student.java

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -2,12 +2,13 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * Changes applied to remote repo
  */
 package studentlist;
 
 /**
  *
- * @author Ronak
+ * @author Sharif Kulmie
  */
 public class Student {
     
